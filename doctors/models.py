7d2f5b4e45d7c459596c389bfad3f84abb17a37c
sanidhya99 <sanidhya738@gmail.com>
2024-08-21 21:43:35 +0530

    altered doctors model

--- doctors/models.py
+++ doctors/models.py
@@ -11,12 +11,12 @@ class DoctorCategory(models.Model):
         return self.name
 
 class Doctors(CustomUser):
-    category=models.ForeignKey(DoctorCategory, related_name="doctor_category", on_delete=models.CASCADE)
+    category=models.ForeignKey(DoctorCategory, related_name="doctor_category", on_delete=models.CASCADE,null=True,blank=True)
     experience = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(1)])  # Adjust the minimum value as needed
-    qualification = models.CharField(max_length=4,default="MBBS")
+    qualification = models.CharField(max_length=4,default="MBBS",null=True,blank=True)
     qualification_doc = models.CharField(max_length=10000,default="MBBS",blank=True,null=True)
     identity_doc = models.CharField(max_length=10000,default="MBBS",blank=True,null=True)
-    price=models.IntegerField()
+    price=models.IntegerField(null=True,blank=True)
     rating=models.IntegerField(default=0)
     rating_no=models.IntegerField(default=0)
     time_slot=ArrayField(ArrayField(models.TimeField(),size=2,default=list),blank=True,null=True,default=list)
