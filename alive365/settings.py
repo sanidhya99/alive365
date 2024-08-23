7521e4d4596fcf441a4a993aaf23f28c4bbbe096
sanidhya99 <100680783+sanidhya99@users.noreply.github.com>
2024-08-24 01:16:48 +0530

    Update settings.py

--- alive365/settings.py
+++ alive365/settings.py
@@ -163,7 +163,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
